ba53d495bee57e41b1930ee27ff3611fe416be57
eskalona17 <eskalona_17@hotmail.com>
2020-04-12 12:01:16 +0200

    creating components

---
+++ src/components/AboutMe.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class AboutMe extends React.Component {
+    render(){
+        return (
+            <>
+                <h1>About Me</h1>
+            </>
+        )
+    }
+}
+
+export default AboutMe
