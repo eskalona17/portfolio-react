be6041ddd5d8c1eff344c4b2490c2df0bf6a9885
eskalona17 <eskalona_17@hotmail.com>
2020-04-12 17:11:19 +0200

    landing page and projects sections created

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,13 +1,36 @@
-import React from 'react'
+import React from "react";
+import { Grid, Cell } from "react-mdl";
 
 class LandingPage extends React.Component {
-    render(){
-        return (
-            <>
-                <h1>Landing Page</h1>
-            </>
-        )
-    }
+  render() {
+    return (
+      <div style={{ width: "100%", margin: "auto" }}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+          <img src="https://img2.freepng.es/20180626/fhs/kisspng-avatar-user-computer-icons-software-developer-5b327cc98b5780.5684824215300354015708.jpg" 
+          alt="avatar"
+          className="avatar-img"
+          />
+          <div className="banner-text">
+              <h1>Full stack web developer</h1>
+              <hr />
+            <p>HTM/CSS | JavaScript | React | NodeJS | MongoDB</p>
+            <div className="social-links">
+                {/* linkedin */}
+                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                    <i className="fa fa-linkedin-square"  aria-hidden="true" />
+                </a>
+                {/* github */}
+                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                    <i className="fa fa-github-square"  aria-hidden="true" />
+                </a>
+            </div>
+          </div>
+          </Cell>
+        </Grid>
+      </div>
+    );
+  }
 }
 
-export default LandingPage
+export default LandingPage;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,164 @@
-import React from 'react'
+import React from "react";
+
+import {
+  Tabs,
+  Tab,
+  Grid,
+  Cell,
+  Card,
+  CardTitle,
+  CardActions,
+  CardText,
+  Button,
+  CardMenu,
+  IconButton,
+} from "react-mdl";
 
 class Projects extends React.Component {
-    render(){
-        return (
-            <>
-                <h1>Projects</h1>
-            </>
-        )
+  state = {
+    activeTab: 0,
+  };
+
+  toggleCategories() {
+    if (this.state.activeTab === 0) {
+      return (
+          <div className="projects-grid">
+        <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+          <CardTitle
+            style={{
+              color: "#fff",
+              height: "176px",
+              background:
+                "url(https://lh3.googleusercontent.com/proxy/5S3laQA9s_8Lj8QMXQXm7j1zXMf8Ih-9oEwo2CaE9wlki9NMlzc62btiGQeCHGJLcuW7zqyEAplvmAOFYyfDoUftmqq1sEXCt8nO2BmsPKdpHXvP2F6IV3ppSHI9kM8lOmkFEUZeow_ko6FJK1zs2MMKwTfhau4wRrVTFef4H3fx19vObYPV) center/cover",
+            }}
+          >React project 1</CardTitle>
+          <CardText>Lorem ipsum askdjajfasjdaldhaljsd</CardText>
+          <CardActions border>
+            <Button colored>Github</Button>
+            <Button colored>Github</Button>
+            <Button colored>Github</Button>
+            <CardMenu style={{color: '#fff'}}>
+                <IconButton name="share"></IconButton>
+            </CardMenu>
+          </CardActions>
+        </Card>
+        <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+          <CardTitle
+            style={{
+              color: "#fff",
+              height: "176px",
+              background:
+                "url(https://lh3.googleusercontent.com/proxy/5S3laQA9s_8Lj8QMXQXm7j1zXMf8Ih-9oEwo2CaE9wlki9NMlzc62btiGQeCHGJLcuW7zqyEAplvmAOFYyfDoUftmqq1sEXCt8nO2BmsPKdpHXvP2F6IV3ppSHI9kM8lOmkFEUZeow_ko6FJK1zs2MMKwTfhau4wRrVTFef4H3fx19vObYPV) center/cover",
+            }}
+          >React project 2</CardTitle>
+          <CardText>Lorem ipsum askdjajfasjdaldhaljsd</CardText>
+          <CardActions border>
+            <Button colored>Github</Button>
+            <Button colored>Github</Button>
+            <Button colored>Github</Button>
+            <CardMenu style={{color: '#fff'}}>
+                <IconButton name="share"></IconButton>
+            </CardMenu>
+          </CardActions>
+        </Card>
+        <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+          <CardTitle
+            style={{
+              color: "#fff",
+              height: "176px",
+              background:
+                "url(https://lh3.googleusercontent.com/proxy/5S3laQA9s_8Lj8QMXQXm7j1zXMf8Ih-9oEwo2CaE9wlki9NMlzc62btiGQeCHGJLcuW7zqyEAplvmAOFYyfDoUftmqq1sEXCt8nO2BmsPKdpHXvP2F6IV3ppSHI9kM8lOmkFEUZeow_ko6FJK1zs2MMKwTfhau4wRrVTFef4H3fx19vObYPV) center/cover",
+            }}
+          >React project 3</CardTitle>
+          <CardText>Lorem ipsum askdjajfasjdaldhaljsd</CardText>
+          <CardActions border>
+            <Button colored>Github</Button>
+            <Button colored>Github</Button>
+            <Button colored>Github</Button>
+            <CardMenu style={{color: '#fff'}}>
+                <IconButton name="share"></IconButton>
+            </CardMenu>
+          </CardActions>
+        </Card>
+        </div>
+        
+      );
+    } else if (this.state.activeTab === 1) {
+      return (
+        <>
+        <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+          <CardTitle
+            style={{
+              color: "#fff",
+              height: "176px",
+              background:
+                "url(https://lh3.googleusercontent.com/proxy/5S3laQA9s_8Lj8QMXQXm7j1zXMf8Ih-9oEwo2CaE9wlki9NMlzc62btiGQeCHGJLcuW7zqyEAplvmAOFYyfDoUftmqq1sEXCt8nO2BmsPKdpHXvP2F6IV3ppSHI9kM8lOmkFEUZeow_ko6FJK1zs2MMKwTfhau4wRrVTFef4H3fx19vObYPV) center/cover",
+            }}
+          >React project 2</CardTitle>
+          <CardText>Lorem ipsum askdjajfasjdaldhaljsd</CardText>
+          <CardActions border>
+            <Button colored>Github</Button>
+            <Button colored>Github</Button>
+            <Button colored>Github</Button>
+            <CardMenu style={{color: '#fff'}}>
+                <IconButton name="share"></IconButton>
+            </CardMenu>
+          </CardActions>
+        </Card>
+        </>
+      );
+    } else if (this.state.activeTab === 2) {
+      return (
+        <>
+        <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+          <CardTitle
+            style={{
+              color: "#fff",
+              height: "176px",
+              background:
+                "url(https://lh3.googleusercontent.com/proxy/5S3laQA9s_8Lj8QMXQXm7j1zXMf8Ih-9oEwo2CaE9wlki9NMlzc62btiGQeCHGJLcuW7zqyEAplvmAOFYyfDoUftmqq1sEXCt8nO2BmsPKdpHXvP2F6IV3ppSHI9kM8lOmkFEUZeow_ko6FJK1zs2MMKwTfhau4wRrVTFef4H3fx19vObYPV) center/cover",
+            }}
+          >React project 3</CardTitle>
+          <CardText>Lorem ipsum askdjajfasjdaldhaljsd</CardText>
+          <CardActions border>
+            <Button colored>Github</Button>
+            <Button colored>Github</Button>
+            <Button colored>Github</Button>
+            <CardMenu style={{color: '#fff'}}>
+                <IconButton name="share"></IconButton>
+            </CardMenu>
+          </CardActions>
+        </Card>
+        </>
+      );
     }
+  }
+
+  render() {
+    return (
+      <>
+        <div className="category-tabs">
+          <Tabs
+            activeTab={this.state.activeTab}
+            onChange={(tabId) => this.setState({ activeTab: tabId })}
+            ripple
+          >
+            <Tab>React</Tab>
+            <Tab>VueJS</Tab>
+            <Tab>MongoDB</Tab>
+          </Tabs>
+          <section className="project-grid">
+            <Grid>
+              <Cell col={12}>
+                <div className="">{this.toggleCategories()}</div>
+              </Cell>
+            </Grid>
+            {/* {this.toggleCategories()} */}
+          </section>
+        </div>
+      </>
+    );
+  }
 }
 
-export default Projects
+export default Projects;
